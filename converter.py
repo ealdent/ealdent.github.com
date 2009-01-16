eb7bbd2584fa7632191fd3f9667c8472641eeeb2
Jason Adams <jasonmadams@gmail.com>
2009-01-15 22:41:38 -0500

    updates to wordpresscom to jekyll converter

--- converter.py
+++ converter.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
+
+
+import date
 import feedparser
+import sys
+
+
 
 
 def load_feed(feed_filename):
+    print "Loading feed %s." % (feed_filename)
     f = open(feed_filename, 'r')
     txt = f.read()
     f.close()
     
+    print "Parsing feed..."
     feed = feedparser.parse(txt)
     
     return feed
@@ -19,3 +28,40 @@ def parse_entry(entry):
     #  tags => list of tags
     #  title => title of post
     #  content => actual post
+    title = entry.title
+    date = date.strptime(entry.wp_post_date, "%Y-%m-%d %H:%M:%S")
+    tags = [tag['term'] for tag in entry.tags if tag['term'] != u'Uncategorized']
+    content = entry.content[0].value
+    link = "_posts/" + entry.link.split(".com")[1].replace("/", "-")
+    
+    print "Processing entry:  %s" % (title)
+    
+    f = open(link, 'w')
+
+    f.write("---\n")
+    f.write("layout: post\n")
+    f.write("title: %s\n" % (title))
+    f.write("tags:\n")
+    for tag in tags:
+        f.write("- %s\n" % (tag))
+    f.write("---\n")
+    f.write(content)
+
+    f.close()
+    
+    print "Finished writing new post:  %s" % (link)
+
+
+
+
+def process_feed(feed):
+    for entry in feed.entries:
+        parse_entry(entry)
+
+
+
+if __name__ == '__main__':
+    feed = load_feed(sys.argv[1])
+    process_feed(feed)
+
+
